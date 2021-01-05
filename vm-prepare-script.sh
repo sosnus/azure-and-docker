af7e48fcc8e500aa232b9a8912c2a4ea6947c412
Sosnus <sosnus15@gmail.com>
2021-01-05 03:17:30 +0100

    Create vm-prepare-script.sh

---
+++ vm-prepare-script.sh
@@ -0,0 +1,13 @@
+# PRZYGOTOWANIE VM DO PRACY Z DOCKEREM
+
+# Instalacja dockera
+sudo snap install docker
+
+### Firewall na Azure
+###Settings -> Networking (dodać potrzebne porty)
+
+# Firewall w systemie (np. Ufw)
+sudo ufw allow 8080
+Sudo ufw allow 22
+Sudo ufw enable
+sudo ufw reload
